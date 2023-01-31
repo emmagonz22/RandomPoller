3cddb4e913ce094a816fde4939ce0a9374e86373
Emmanuel Gonzalez Morales <emmanuel_gonzalez@techexchange.in>
2023-01-31 16:26:45 -0400

    [FEAT] __next__ fixed, stop implemented and basic methods working

--- poller.py
+++ poller.py
@@ -1,6 +1,7 @@
 import os 
 import csv
 import random
+import sys
 
 class Participant:
     ##<name>,<#polled>,<#correct>,<#attempted>,<#excused>
@@ -14,7 +15,7 @@ class Participant:
     def __str__(self):
         return f"{self.name},{self.polled_counter},{self.correct_counter},{self.attempted_counter},{self.excused_counter}"
 
-    def participate(self):
+    def increase_attempted_counter(self):
         self.attempted_counter += 1
 
     def increase_correct_counter(self):
@@ -32,17 +33,20 @@ class Poller:
         self.filename : str = filename
         self.participants = []
         self.execution_attempteds = 0
+        self.selected_participant = None
     def __enter__(self):
-        if os.stat("./data/participant.csv").st_size == 0:
+        if os.stat(self.filename).st_size == 0:
             raise ValueError("File is empty")
         else:
             with open(self.filename, "r") as filecsv:
                 participant_csvfile = csv.reader(filecsv, delimiter=" ")
-                print(participant_csvfile)
+          
                 for participant in participant_csvfile:
+                    print(participant)
                     explode_line = participant[1].split(',')                
-                    self.participants.append(  Participant(f"{participant[0]} {explode_line[0]}",explode_line[1],explode_line[2],explode_line[3],explode_line[4])  )
-        
+                    self.participants.append(  Participant(f"{participant[0]} {explode_line[0]}",int(explode_line[1]),int(explode_line[2]), int(explode_line[3]), int(explode_line[4]) ))
+        return self
+    
     def __exit__(self, exc_type, exc_val, exc_tb):
         formated_list = []
         
@@ -50,47 +54,75 @@ class Poller:
             participant_lst = [participant.name, participant.polled_counter, participant.correct_counter, participant.attempted_counter, participant.excused_counter]
             formated_list.append(participant_lst)
 
-        with open('../data/participants.csv', 'w') as writeFile:
+        with open(self.filename, 'w') as writeFile:
                 writer = csv.writer(writeFile)
                 writer.writerows(formated_list)
 
         #self.participant_csvfile.close()
     def __iter__(self):
-        self.current_index = 0
+        self.random_index = -1
         return self
     def __next__(self): #Fix to select random participant
 
-
         polled_num_set = set()
         for participant in self.participants:
-            explode = participant[1].split(',')
-            polled_num_set.add(int(explode[1]))
+           
+            polled_num_set.add(int(participant.polled_counter))
                 
         min_poll_num = min(polled_num_set)
+      
         qualified_participants = []
         for participant in self.participants:
-            if int(participant[2]) == min_poll_num:
+            if int(participant.polled_counter) == min_poll_num:
+
                 qualified_participants.append(participant)
-
+    
         if (len(qualified_participants) > 0):
-            random_participant_index = random.randint(0,len(qualified_participants)-1)
-            selected_participant = qualified_participants[random_participant_index]
+            self.random_index = random.randint(0,len(qualified_participants)-1)
+            self.selected_participant = qualified_participants[self.random_index]
+            self.execution_attempteds += 1
         else:
-            selected_participant = None
-
-        return selected_participant
-    def select_random_participant(self):
-        pass
+            self.selected_participant = None
+        
+        return self.selected_participant
     def correct(self):
-        pass
+        if self.random_index != -1:
+            self.selected_participant.increase_correct_counter()
+            self.selected_participant.increase_polled_counter()
+            self.random_index = -1
+        else:
+            raise Exception("Index of participant not found")
+    
+    """Increment the Participant correct value by one.
+
+    Increment the correct value of the Participant in random_index in the participants list
+
+    Raises:
+        Exception: Index of participant not found.
+    """
     def attempted(self):
-        pass
+        print(self.random_index, self.participants[self.random_index])
+        if self.random_index != -1:
+            self.selected_participant.increase_attempted_counter()
+            self.selected_participant.increase_polled_counter()
+            self.random_index = -1
+        else:
+            raise Exception("Index of participant not found")
     def excused(self):
-        pass
+        if self.random_index != -1:
+            self.selected_participant.increase_excused_counter()
+            self.selected_participant.increase_polled_counter()
+            self.random_index = -1
+        else:
+            raise Exception("Index of participant not found")
     def missing(self):
-        pass    
-    def stop(self): #Research
-        pass
+        if self.random_index != -1:
+            self.selected_participant.increase_polled_counter()
+            self.random_index = -1
+        else:
+            raise Exception("Index of participant not found")
+    def stop(self): 
+        sys.exit("End of program")
     def total(self):
   
         for participant in self.participants:
@@ -98,8 +130,6 @@ class Poller:
 
 p = Poller("./data/participants.csv")
 p.total()
-p.stop()
-
 
 '''
 
